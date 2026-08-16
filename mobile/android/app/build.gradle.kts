76476d4e63a9522f4d8ae3538481fd2bac3db745
特昂糖糖糖 <126449209+teangtang1122@users.noreply.github.com>
2026-08-16 23:18:40 +0800

    chore: bump Android version to 3.2.3

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.siming.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30202
-        versionName = "3.2.2"
+        versionCode = 30203
+        versionName = "3.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
